9283fb012b31ffd3154618d48d1a93333b649033
jawnhoang <johnhoang5@gmail.com>
2021-03-25 19:59:21 -0700

    web server works

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 148webServer
+learning objectives:
+    -basics of TCP socket programming
+        -how to create socket
+        -bind to specific addr and port
+        -send/receive HTTP packet
+        -basic HTTP header format
--- hw2.py
+++ hw2.py
@@ -2,24 +2,30 @@
 from socket import *
 import sys #in order to terminate program
 
+#welcoming socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #prepare a serverSocket
-#to do
-
-
+serverPort = 80  #80 port for http servers
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print("using port number:", serverPort)
 
 while True:
     #establish connection
     print('Ready to server...')
-    connectionSocket, addr =
+    connectionSocket, addr = serverSocket.accept()
     try:
-        message =
+        message = connectionSocket.recv(1024).decode()  #read bytes from socket
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata =
+        outputdata = f.read()                           #read page
 
         #send one HTTP header line into serverSocket
         ## TODO:
+        #on success
+        connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
+        connectionSocket.send(outputdata.encode())
+        print("data from the page: ", outputdata)
 
         #send the content of requested file to the client
         for i in range(0, len(outputdata)):
@@ -32,12 +38,11 @@ while True:
     except IOError:
         #send response message for file not found
         ## TODO:
-
-
+        connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n'.encode())
         #close client serverSocket
         ## TODO:
+        connectionSocket.close()
 
     serverSocket.close()
     #terminate the program after sending coreesponding data
     sys.exit()
-        
